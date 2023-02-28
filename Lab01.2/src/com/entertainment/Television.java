e5e60fa3763d1af0d69aa3aa248f27104618efd0
Brandon(Suin) Park <Suin.Park@tlgcohort.com>
2023-02-28 10:15:14 -0800

    Refinement of equals

--- Lab01.2/src/com/entertainment/Television.java
+++ Lab01.2/src/com/entertainment/Television.java
@@ -45,18 +45,64 @@ public class Television {
         this.volume = volume;
     }
 
+/*    @Override
+    public boolean equals(Object obj) {
+        // if television object same as the physical object as obj
+        if (this == obj) return true;
+
+        // if obj is null OR I and obj are not the same EXACT type
+        if (obj == null || this.getClass() != obj.getClass()) return false;
+        Television that = (Television) obj;
+        return this.getVolume() == that.getVolume() &&
+                    Objects.equals(this.getBrand(), that.getBrand());
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getBrand(), getVolume());
+    }
+*/
+    /*
+     * @Override
+     * public boolean equals(Object obj) {
+     * boolean result = false;
+     * // this (me) and obj refer to the same physical object in memory!
+     * if (this == obj) {
+     * result = true;
+     * // obj is not null nd my class object is the same as its class object
+     * else if (obj != null && (this.getClass() == obj.getClass())) {
+     * Television other = (Television) obj;
+     * result = Objects.equals(this.getBrand(), other.getBrand()) &&
+                     this.getVolume() == other.getVolume();
+     * } return result; }
+     */
+
+    @Override
+    public int hashCode() {
+        // this is a poorly written hash function, because it easily results in "has collisions"
+        // a "hash collision" is when DIFFERENT objects (by equals())
+        // have the SAME hashcode (just by coincidence or dumb luck)
+        // return getBrand().length() + getVolume();
+
+        // we can use java.util.Objects to give us a "scientifically correct" hash function,
+        // i.e., one that minimizes the probability of hash collisions
+        return Objects.hash(getBrand(), getVolume());
+
+    }
+
     @Override
     public boolean equals(Object obj) { // Television is an Object
         boolean result = false;
 
-        // proceed only if 'obj' is reall a reference to a Television object
-        if(obj instanceof Television) {
+        // proceed only if 'obj' is really a reference to a Television object
+        if(obj != null && (this.getClass() == obj.getClass())) {  // instanceof is a IS-A check change to Class object. okay to use == for Class object
             // downcast to more specific type Television, so we can call Television methods
             Television other = (Television) obj;
 
             // do the checks: brands are the same AND volumes are the same
             result = Objects.equals(this.getBrand(), other.getBrand()) && // null-safe check
-                     this.getVolume() == other.getVolume();  // int can't be null
+                     this.getVolume() == other.getVolume();               // int can't be null
         }
         return result;
     }
