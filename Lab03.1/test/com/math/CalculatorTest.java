95eef9ac2807df0fd8661aba0ec54a78a2b9db58
Brandon(Suin) Park <Suin.Park@tlgcohort.com>
2023-02-28 15:26:31 -0800

    Lab 03.1 Complete

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -9,13 +9,38 @@
 package com.math;
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class CalculatorTest {
 
-  @Test
-  public void testAdd() {
-    Calculator calc = new Calculator();
-    assertEquals(5, calc.add(1, 4));  // expected, actual
-  }
+    @Test
+    public void testAdd() {
+        System.out.println("testAdd");
+
+        Calculator calc = new Calculator();
+        int result = calc.add(1, 4);
+
+        assertEquals(5, result);  // expected, actual
+    }
+
+    @Test
+    public void testDivide() {
+        System.out.println("testDivide");
+
+        Calculator calc = new Calculator();
+        double result = calc.divide(5, 2);
+
+        assertEquals(2.5, result, .001); // expected, actual, delta
+    }
+
+    @Test
+    public void testIsEven() {
+        System.out.println("testIsEven");
+
+        Calculator calc = new Calculator();
+
+        assertTrue(calc.isEven(10));
+        assertFalse(calc.isEven(11));
+    }
 }
