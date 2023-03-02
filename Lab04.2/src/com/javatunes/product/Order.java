f0dae46ab0b852ca80f4ff893821e6c6ede5fbfc
Brandon(Suin) Park <Suin.Park@tlgcohort.com>
2023-03-02 14:50:09 -0800

    Lab 4.2 complete (MiniLab 4.1)

--- Lab04.2/src/com/javatunes/product/Order.java
+++ Lab04.2/src/com/javatunes/product/Order.java
@@ -11,22 +11,28 @@ package com.javatunes.product;
 import java.util.Collection;
 
 public class Order {
-  private String id;
-  
-  public Order(String id) {
-    this.id = id;
-  }
-  
-  /**
-   * TODO:
-   * get the items from the cart and iterate over them, print each item's product code
-   * get cart total and print
-   */
-  public void processCart(Object reference_to_cart) {
-    
-  }
-  
-  public String getId() {
-    return id;
-  }
+    private String id;
+
+    public Order(String id) {
+        this.id = id;
+    }
+
+    /**
+     * TODO:
+     * get the items from the cart and iterate over them, print each item's product code
+     * get cart total and print
+     */
+    // public void processCart(ShoppingCart<? extends Products> cart)
+    public <T extends Product> void processCart(ShoppingCart<T> cart) {
+        System.out.println("Order " + getId() + " contains the following:");
+
+        for (Product product : cart.allItems()) {
+            System.out.println(product.getCode());
+        }
+        System.out.println(cart.total());
+    }
+
+    public String getId() {
+        return id;
+    }
 }
--- Lab04.2/src/com/javatunes/product/ShoppingCart.java
+++ Lab04.2/src/com/javatunes/product/ShoppingCart.java
@@ -12,15 +12,40 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
-public class ShoppingCart<T> {
-  // storage for the cart's contents
-  private Collection<T> items = new ArrayList<T>();  // diamond not used here just to emphasize the T
-  
-  public void addItem(T item) {
-    items.add(item);
-  }
-  
-  public void removeItem(T item) {
-    items.remove(item);
-  }
+
+/*
+ * The "T" is really going to be something like MusicItem
+ * if ShoppingCart<MusicItem> cart = ...      // what's the T? MusicItem
+ * if ShoppingCart<MediaPlayer> cart = ... // what's the T? MediaPlayer
+ */
+public class ShoppingCart<T extends Product> {
+    // storage for the cart's contents
+    private Collection<T> items = new ArrayList<T>();  // diamond not used here just to emphasize the T
+
+    public double total() {
+        double result = 0.0;
+
+        for (T item : items) {
+            result += item.getPrice();
+        }
+        return result;
+    }
+
+    public Collection<T> allItems() {
+        return Collections.unmodifiableCollection(items);
+    }
+
+    public int size() {
+        return items.size();
+    }
+
+    public void addItem(T item) {
+        items.add(item);
+    }
+
+    public void removeItem(T item) {
+        items.remove(item);
+    }
+
+
 }
